a1f11c75639089827d7d0599bf337c0c78a877fb
Craig Richards <craig@geekcomputers.co.uk>
2012-06-26 15:39:37 +0100

    Backup MySQL database, either the full database or just a single database

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
-# Script Name    : mysql_backup.sh
-
-# Author         : Craig Richards
-# Created        : 08-March-2012
+# Script Name	: mysql_backup.sh
+# Author			: Craig Richards
+# Created			: 08-March-2012
+# Last Modified	:
+# Version			: 1.0
 
 # Modifications  : 
 
-# Description    : Backup MySQL database, either the full database or just a single database
-
-# Instructions   : mysql_backup.sh full or mysql_backup.sh db db_name
+# Description		: Backup MySQL database, either the full database or just a single database
 
 #################################
 # Start of procedures/functions #
 #################################
 
-funct_check_params()				# Function Name
-{						# Start of the function
-  if [ ${NARG} -lt 1 ]; then			# If the number of arguments is not one, then output a message
+funct_check_params()									# Function Name
+{																	# Start of the function
+  if [ ${NARG} -lt 1 ]; then								# If the number of arguments is not one, then output a message
     echo "Usage: $0 argument"
     echo "full = Full MySQL Backup"
     echo "db dbname = Single Database backup"
-    exit 1					# Quit the program
+    exit 1														# Quit the program
   elif
 
   # If the argument passed is -h or --h then display the following message in the echo statement
@@ -30,27 +29,27 @@ funct_check_params()				# Function Name
     echo "Usage: $0 argument"
     echo "full = Full MySQL Backup"
     echo "db dbname = Single Database backup"
-    exit 1					# Quit the program
-  fi						# End of the if statement
-}						# End of the function
+    exit 1														# Quit the program
+  fi																# End of the if statement
+}																	# End of the function
 
-funct_backup()					# Function Name
-{						# Start of the function
-  if [ ${choice} = "full" ]; then               # If the choice is full, then..
-    mkdir -p ${BACKUP_DIR}			# Make the backup directory
+funct_backup()												# Function Name
+{																	# Start of the function
+  if [ ${choice} = "full" ]; then 							# If the choice is full, then..
+    mkdir -p ${BACKUP_DIR}							# Make the backup directory
 
     # Run the command to backup ALL the databases in MySQL, timestamp the file to the dated folder
 
     mysqldump --all-databases | gzip > ${BACKUP_DIR}/full_mysql_backup-`date +%H:%M`.gz
-  elif						# Or
-  [[ ${choice} = "db" ]]; then			# If the choice is db, then..
-    mkdir -p ${BACKUP_DIR}			# Make the backup directory
+  elif																# Or
+  [[ ${choice} = "db" ]]; then							# If the choice is db, then..
+    mkdir -p ${BACKUP_DIR}							# Make the backup directory
 
     # Backup just the given database, timestamp the file to the dated folder
 
     mysqldump ${dbbkup} | gzip > ${BACKUP_DIR}/${dbbkup}_mysql_backup-`date +%H:%M`.gz
   fi  
-}						# End of the function
+}																	# End of the function
 
 
 ################
@@ -60,14 +59,14 @@ funct_backup()					# Function Name
 # Variable Settings
 
 DATE=`date +"%d-%B-%Y"` ; export DATE		# Set the DATE variable, format it as 20-February-2012
-choice=$1					# Set the variable choice as the first argument passed
-dbbkup=$2					# Set the variable dbbkup as the database to backup
-NARG=$#						# Set the variable NARG to a number of arguments on the command line
+choice=$1														# Set the variable choice as the first argument passed
+dbbkup=$2													# Set the variable dbbkup as the database to backup
+NARG=$#														# Set the variable NARG to a number of arguments on the command line
 
-{						# Start of the main program
-  funct_check_params				# Call the function funct_check_params
-  funct_backup					# Call the function funct_backup
-}						# End of the main program
+{																	# Start of the main program
+  funct_check_params									# Call the function funct_check_params
+  funct_backup												# Call the function funct_backup
+}																	# End of the main program
 
 ## End of Script
 
